daa4606bfbb0ed24d379f8010888f0d00d0b1716
kar-re <kaspianjakobsson@gmail.com>
2023-05-14 18:39:39 +0200

    Some kind of try for parser

--- assignment3/Parser.hs
+++ assignment3/Parser.hs
@@ -4,30 +4,44 @@ module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
 import Prelude hiding (return, fail)
 import Data.Char
 import CoreParser
-infixl 7 -#, #- 
+infixl 7 -#, #-
 
 type T a = Parser a
 
 err :: String -> Parser a
 err message cs = error (message++" near "++cs++"\n")
 
-iter :: Parser a -> Parser [a]  
-iter m = m # iter m >-> cons ! return [] 
+iter :: Parser a -> Parser [a]
+iter m = m # iter m >-> cons ! return []
 
 cons(a, b) = a:b
 
+-- The parser m -# n accepts the same input as m # n, but returns just the result from the n parser. 
+-- The function should be declared as a left associative infix operator with precedence 7. Example:
 (-#) :: Parser a -> Parser b -> Parser b
-m -# n = error "-# not implemented"
+m -# n = m # n >-> snd
+-- m -# n = error "-# not implemented"
+
+-- >>> (accept "read" -# word) "read count;" 
+-- Nothing
+
 
 (#-) :: Parser a -> Parser b -> Parser a
-m #- n = error "#- not implemented"
+m #- n = m # n >-> fst
 
+
+
+space :: Parser Char
+space = char ? isSpace
+
+-- spaces accepts any number of whitespace characters as defined by the Prelude function isSpace.
 spaces :: Parser String
-spaces =  error "spaces not implemented"
+spaces = space # iter space >-> cons
 
 token :: Parser a -> Parser a
 token m = m #- spaces
 
+-- letter is a parser for a letter as defined by the Prelude function isAlpha.
 letter :: Parser Char
 letter = char ? isAlpha
 -- letter =  error "letter not implemented"
@@ -35,20 +49,25 @@ letter = char ? isAlpha
 word :: Parser String
 word = token (letter # iter letter >-> cons)
 
+-- The parser chars n accepts n characters.
 chars :: Int -> Parser String
-chars n =  error "chars not implemented"
+-- chars n =  error "chars not implemented"
+chars n = char # iter char >-> cons
+-- chars n =  error "chars not implemented"
 
 accept :: String -> Parser String
 accept w = (token (chars (length w))) ? (==w)
 
+-- The parser require w accepts the same string input as accept w but reports the missing string using err in case of failure.
 require :: String -> Parser String
-require w  = error "require not implemented"
+require w = accept w ! error "error " 
+-- require w  = error "require not implemented"
 
 lit :: Char -> Parser Char
 lit c = token char ? (==c)
 
-digit :: Parser Char 
-digit = char ? isDigit 
+digit :: Parser Char
+digit = char ? isDigit
 
 digitVal :: Parser Integer
 digitVal = digit >-> digitToInt >-> fromIntegral
